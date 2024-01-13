f81de920fe17e85655a905ef3fe0272892832297
Avinash V K <avinash2000vk@gmail.com>
2024-01-14 00:24:58 +0530

    Completed the landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmsans: ['"DM Sans"', "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        poly: ["Poly", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
